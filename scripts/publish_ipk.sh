9738f96f6eeca05d778d2e0635306fa76bb40b7c
Tomas Novotny <tomas@novotny.cz>
2016-05-04 14:29:08 +0200

    publish_ipk: upload locale and reformat
    Excluding of locale had a side effect - perl-module-locale-* weren't
    uploaded. So upload all locale packages, they are not so big.
    
    Signed-off-by: Tomas Novotny <tomas@novotny.cz>

--- scripts/publish_ipk.sh
+++ scripts/publish_ipk.sh
@@ -3,4 +3,16 @@
 # TN, 11/2013
 
 bitbake package-index
-rsync -rlv --delete --include "beeeon-*-dbg*" --include "fitprotocold-dbg*" --include "*libgcc-s-dbg*" --include "*libpoco-dbg*" --exclude="i686-nativesdk/" --exclude="x86_64-nativesdk/" --exclude="*-dbg_*" --exclude="*-doc_*" --exclude="*-locale-*" --exclude="*-localedata-*" --include "libc6-dbg*" /home/tom/oe/build/jethro-glibc/deploy/ipk/ tom@cloud.beeeon.com:/var/www/html/feed_jethro/
+rsync -rlv --delete \
+	--include "beeeon-*-dbg*" \
+	--include "fitprotocold-dbg*" \
+	--include "*libgcc-s-dbg*" \
+	--include "*libpoco-dbg*" \
+	--exclude="i686-nativesdk/" \
+	--exclude="x86_64-nativesdk/" \
+	--exclude="*-dbg_*" \
+	--exclude="*-doc_*" \
+	--exclude="*-localedata-*" \
+	--include "libc6-dbg*" \
+	/home/tom/oe/build/jethro-glibc/deploy/ipk/ \
+	tom@cloud.beeeon.com:/var/www/html/feed_jethro/
